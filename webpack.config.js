2d0002a544f2f786d5fbff569c594aa0dc44bce5
Barnicle <egalimov95@gmail.com>
2020-05-24 20:29:03 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  devServer: {
+    contentBase: "./build",
+    port: 3000,
+    hot: true,
+    open: true,
+    clientLogLevel: "silent",
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: false,
+      appMountId: "root",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(png|jpg|svg|ttf)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              context: path.resolve(__dirname, "images/"),
+              outputPath: "img/",
+              // useRelativePaths: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+            options: { injectType: "singletonStyleTag" },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
